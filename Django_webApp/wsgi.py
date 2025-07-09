2c77d615ca7dbf5d4925ed1df58cf43a89a30e8c
z-ABYa <189abhish@gmail.com>
2025-07-09 21:09:16 +0530

    Update wsgi.py

--- Django_webApp/wsgi.py
+++ Django_webApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_project project.
+WSGI config for Django_webApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_webApp.settings')
 
 application = get_wsgi_application()
 
